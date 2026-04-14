85aa30f6661fea9b2e459e812fce3012c3288f3f
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2026-04-14 10:42:48 +0300

    Update conv_block.py

--- src/blocks/conv_block.py
+++ src/blocks/conv_block.py
@@ -1,7 +1,14 @@
+import torch
 import torch.nn as nn
 
-def conv_block(in_ch, out_ch, kernel=3, padding=1):
-    return nn.Sequential(
-        nn.Conv2d(in_ch, out_ch, kernel_size=kernel, padding=padding),
-        nn.ReLU(inplace=True)
-    )
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_ch, out_ch, kernel_size=3, stride=1, padding=1):
+        super().__init__()
+        self.block = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, kernel_size=kernel_size, stride=stride, padding=padding, bias=True),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.block(x)
